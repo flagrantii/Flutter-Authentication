1bace741e0b4f1216836da4fa37ace2c9282195b
flagrantii <tanuson679@gmail.com>
2024-03-11 16:26:00 +0700

    Create Register Form

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/model/profile.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -8,6 +9,9 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final formKey = GlobalKey<FormState>();
+  Profile profile = Profile(email: '', password: '');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,29 +19,54 @@ class _RegisterScreenState extends State<RegisterScreen> {
           title: const Text('Register'),
         ),
         body: Container(
-          child: Form(
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'Enter your email',
-                  ),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              key: formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Enter your email',
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      onSaved: (String? value) {
+                        profile.email = value!;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter your password',
+                      ),
+                      obscureText: true,
+                      onSaved: (String? value) {
+                        profile.password = value!;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          formKey.currentState?.save();
+                          print(
+                              "email = ${profile.email} , password = ${profile.password}");
+                          formKey.currentState?.reset();
+                        },
+                        child: const Text('Register'),
+                      ),
+                    ),
+                  ],
                 ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    hintText: 'Enter your password',
-                  ),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Register'),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ));
