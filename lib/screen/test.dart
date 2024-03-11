a00a810e9089512ed993cbe0d8c283802ed52d0a
flagrantii <tanuson679@gmail.com>
2024-03-11 15:18:56 +0700

    Create Flutter Porject

--- lib/screen/test.dart
+++ lib/screen/test.dart
@@ -389,3 +389,787 @@
 //     return const Placeholder();
 //   }
 // }
+
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+// import 'package:flutter/material.dart';
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
